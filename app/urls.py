f9eceb73cbc93a32e760f37b08258f3abf3b1dd5
YAHYA-H <husain.host@gmail.com>
2019-03-22 11:48:48 +0300

    add user auth

--- app/urls.py
+++ app/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from graphene_django.views import GraphQLView
-from app.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
---
+++ sevenapp/schema.py
@@ -0,0 +1,38 @@
+import graphene
+from graphene_django import DjangoObjectType
+from .models import User
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
+
+class Query(graphene.ObjectType):
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return User.objects.all()
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
