3a01ac0622e75b262d57349f440d29c4075d44ca
YAHYA-H <husain.host@gmail.com>
2019-03-25 12:41:24 +0300

    add homepage template

--- app/urls.py
+++ app/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import url
-from graphene_django.views import GraphQLView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('', include('sevenapp.urls')),
+
 ]
--- sevenapp/views.py
+++ sevenapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """ Render homepage"""
+    return render(request, 'index.html')
