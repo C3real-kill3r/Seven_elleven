4601bf03bb88204260aa24f154ce13cc4acdd0f0
C3real-kill3r <brybzi@gmail.com>
2019-03-30 15:39:09 +0300

    feat(authentication): set up login and sign up endpoints
    - add mutation for registering a user to seven elleven
    - add mutation for authenticating a user when loging in
    
    [Finishes #164952990]

--- app/schema.py
+++ app/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import graphql_jwt
 import app.sevenelleven.authentication.schema
 
 
@@ -7,7 +8,9 @@ class Query(app.sevenelleven.authentication.schema.Query, graphene.ObjectType):
 
 
 class Mutation(app.sevenelleven.authentication.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
