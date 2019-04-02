5a6651127968e8438865e1ea96472f73d80210f0
C3real-kill3r <brybzi@gmail.com>
2019-04-02 13:08:26 +0300

    fix(authentication): return token on successful authentication
    - add serializers file
    - add a middleware file in the authentication app
    - refactor login mutation to return token after successful login
    
    [Fixes #165054587]

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,4 @@
 import graphene
-import graphql_jwt
 import app.sevenelleven.authentication.schema
 
 
@@ -8,9 +7,7 @@ class Query(app.sevenelleven.authentication.schema.Query, graphene.ObjectType):
 
 
 class Mutation(app.sevenelleven.authentication.schema.Mutation, graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
+    pass
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
